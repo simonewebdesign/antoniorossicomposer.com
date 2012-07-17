751266c7d12fe44e81ffd7191d68b2bcc6ee47cc
SimoPcLorena <simo@pclorena>
2012-07-17 18:31:06 +0200

    added microdata to events

--- application/includes/event.php
+++ application/includes/event.php
@@ -1,6 +1,7 @@
-<div class=event>
-	<span class=place><?=$event->place?></span> - 
-	<span class=date><?=$event->theDatetime?></span>
-	<h3 class=title><?=$event->title?></h3>
-	<span class=description><?=$event->description?></span>
+<div class=event itemscope itemtype="http://data-vocabulary.org/Event" id=event<?=$event->id?>>
+	<meta itemprop=url content="<?=ROOT . $lang . '/event' . $event->id?>">
+	<span class=place itemprop="location" itemscope itemtype="http://data-vocabulary.org/​Organization"><?=$event->place?></span> - 
+	<span class=date itemprop="startDate" datetime="<?=$event->startDate?>"><?=$event->theDatetime?></span>
+	<h3 class=title itemprop="summary"><?=$event->title?></h3>
+	<span class=description itemprop="description"><?=$event->description?></span>
 </div>
--- application/includes/pages/it/4-eventi.php
+++ application/includes/pages/it/4-eventi.php
@@ -6,7 +6,7 @@ $en_format_string = "%D %r";
 
 $format_string = $lang == 'it' ? DATE_FORMAT_DATETIME : $en_format_string;
 
-$events_query = "SELECT title_$lang as `title`, description_$lang as `description`, place_$lang as `place`, date_format(eventDate, '$format_string') as theDatetime FROM events ORDER BY eventDate DESC";
+$events_query = "SELECT id, title_$lang as `title`, description_$lang as `description`, place_$lang as `place`, date_format(eventDate, '$format_string') as theDatetime, date_format( eventDate, '%m-%d-%YT%d:%i-08:00' ) AS startDate FROM events ORDER BY eventDate DESC";
 
 $events = $db->query($events_query);
 
