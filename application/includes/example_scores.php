0dfb4fd077f88e01ca8ae59ea5bac4f4d9130321
SimoPcLorena <simo@pclorena>
2012-07-19 19:35:31 +0200

    scores ebbasta

--- application/includes/example_scores.php
+++ application/includes/example_scores.php
@@ -7,18 +7,22 @@ $scores = scandir($scores_path);
 
 <hr>
 
-<h3><?=$scores_title?></h3>
+<section id=scores class=clearfix>
 
-<ul>
-<?php
-foreach ($scores as $score) {
-	if ( is_file($scores_path.$score) ) {
-?>
-	<li>
-		<a href="<?=$scores_path.$score?>"><?=str_replace('.pdf','',$score)?></a>
-	</li>
-<?php
+	<h3><?=$scores_title?></h3>
+
+	<ul>
+	<?php
+	foreach ($scores as $score) {
+		if ( is_file($scores_path.$score) ) {
+	?>
+		<li>
+			<a href="<?=$scores_path.$score?>"><?=str_replace('.pdf','',$score)?></a>
+		</li>
+	<?php
+		}
 	}
-}
-?>
-</ul>
+	?>
+	</ul>
+
+</section>
